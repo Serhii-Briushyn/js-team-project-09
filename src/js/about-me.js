6ef88ad6a649f582f5ef4783b5e014989d2c02d8
Serhii Briushyn <serhii.briushyn@icloud.com>
2024-08-17 22:34:16 +0200

    about-me-markup

---
+++ src/js/about-me.js
@@ -0,0 +1,57 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+
+document.querySelectorAll('.details-btn').forEach(button => {
+  button.addEventListener('click', function () {
+    const item = this.closest('.details-item');
+    item.classList.toggle('active');
+    const content = item.querySelector('.details-content');
+    if (item.classList.contains('active')) {
+      content.style.maxHeight = content.scrollHeight + 'px';
+    } else {
+      content.style.maxHeight = 0;
+    }
+  });
+});
+
+const firstItem = document.querySelector(
+  '.details-item.active .details-content'
+);
+if (firstItem) {
+  firstItem.style.maxHeight = firstItem.scrollHeight + 'px';
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  let swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 0,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+    },
+
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+
+    breakpoints: {
+      375: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1440: {
+        slidesPerView: 6,
+      },
+    },
+  });
+
+  document
+    .querySelector('.swiper-button-next')
+    .addEventListener('click', () => {
+      swiper.slideNext();
+    });
+});
