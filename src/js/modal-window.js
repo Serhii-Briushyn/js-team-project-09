e6f196c508e3a0ce6ac906f843a42bc6eaac9c0b
Vladusslav <vladfedunetx@gmail.com>
2024-08-21 17:42:06 +0300

    modal-window

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -1,6 +1,7 @@
 const closeButton = document.querySelector('.mobile-menu-close-btn');
 const burgerBtn = document.querySelector('.header-mobile-open-btn');
 const menu = document.querySelector('.mobile-menu');
+const links = document.querySelectorAll('.mob-menu-list-link');
 
 closeButton.addEventListener('click', () => {
   menu.classList.toggle('is-open');
@@ -9,3 +10,9 @@ closeButton.addEventListener('click', () => {
 burgerBtn.addEventListener('click', () => {
   menu.classList.toggle('is-open');
 });
+
+links.forEach(link => {
+  link.addEventListener('click', (e) => {
+    menu.classList.remove('is-open');
+  })
+})
