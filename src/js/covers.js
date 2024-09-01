5d003d96092c3f049d67df884a3320a206290e73
Serhii Briushyn <serhii.briushyn@icloud.com>
2024-09-01 15:40:55 +0200

    section update

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,64 +1,28 @@
-const viewport = document.querySelector('.cover-section');
-
-function isCenterInViewport(element) {
-  const rect = element.getBoundingClientRect();
-  const viewportHeight = window.innerHeight;
-  const viewportWidth = window.innerWidth;
-  const elementCenterX = rect.left + rect.width / 2;
-  const elementCenterY = rect.top + rect.height / 2;
-
-  return (
-    elementCenterX >= 0 &&
-    elementCenterX <= viewportWidth &&
-    elementCenterY >= 0 &&
-    elementCenterY <= viewportHeight
-  );
-}
-
-function checkScroll() {
-  if (isCenterInViewport(viewport)) {
-    if (!viewport.classList.contains('scrolling')) {
-      console.log('Section is in viewport".');
-    }
-    viewport.classList.add('scrolling');
-  } else {
-    if (viewport.classList.contains('scrolling')) {
-      console.log('Section is out of viewport".');
-    }
-    viewport.classList.remove('scrolling');
-  }
-}
-
-function debounce(func, wait) {
-  let timeout;
-  return function (...args) {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => func.apply(this, args), wait);
+document.addEventListener('DOMContentLoaded', function () {
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1,
   };
-}
 
-const debouncedCheckScroll = debounce(checkScroll, 100);
-window.addEventListener('scroll', debouncedCheckScroll);
-
-checkScroll();
-
-// *   Animation;
-
-document.addEventListener('DOMContentLoaded', () => {
-  const coverLists = document.querySelectorAll('.cover-list li');
-
-  const listDelay = 500;
-
-  coverLists.forEach((list, listIndex) => {
-    list.style.transitionDelay = `${listIndex * listDelay}ms`;
-    list.classList.add('show');
-
-    const listItems = list.querySelectorAll('li');
-
-    listItems.forEach((item, itemIndex) => {
-      const itemDelay = 300;
-      item.style.transitionDelay = `${itemIndex * itemDelay}ms`;
-      item.classList.add('show');
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const pictures = entry.target.querySelectorAll('.cover-picture');
+        pictures.forEach(picture => {
+          picture.classList.add('animate-marquee');
+        });
+      } else {
+        const pictures = entry.target.querySelectorAll('.cover-picture');
+        pictures.forEach(picture => {
+          picture.classList.remove('animate-marquee');
+        });
+      }
     });
-  });
+  }, options);
+
+  const wrap = document.querySelector('.cover-general-wrap');
+  if (wrap) {
+    observer.observe(wrap);
+  }
 });
--- src/main.js
+++ src/main.js
@@ -9,3 +9,22 @@ import './js/work-together.js';
 window.onload = function () {
   window.scrollTo(0, 0);
 };
+
+let scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = 'block';
+  } else {
+    scrollToTopBtn.style.display = 'none';
+  }
+}
+
+scrollToTopBtn.onclick = function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
